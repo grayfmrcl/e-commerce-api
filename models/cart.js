1fef88d0389ff3b64c738859dc01dffc1de3dd1b
Grayfield <grayfieldmm@outlook.com>
2018-09-04 18:59:54 +0700

    add to cart

---
+++ models/cart.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const cartSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  items: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      quantity: { type: Number, default: 1 },
+    },
+  ],
+});
+
+cartSchema.statics.addItem = function addItem(userId, product, quantity = 1) {
+  return new Promise((resolve, reject) => {
+    this.findOne({ user: userId })
+      .then((cart) => {
+        if (!cart) {
+          this.create({
+            user: userId,
+            items: [{ product, quantity }],
+          });
+          resolve(cart.save());
+        } else {
+          const item = cart.items.find(i => i.product.equals(product));
+          if (!item) {
+            cart.items.push({ product, quantity });
+          } else {
+            item.quantity += quantity || 1;
+          }
+          resolve(cart.save());
+        }
+      })
+      .catch(err => reject(err));
+  });
+};
+
+module.exports = mongoose.model('Cart', cartSchema);
--- routers/product_router.js
+++ routers/product_router.js
@@ -9,5 +9,6 @@ router.get('/:id', product.findProduct);
 router.post('/', authenticate, authorize(['admin']), product.addProduct);
 router.put('/:id', authenticate, authorize(['admin']), product.editProduct);
 router.delete('/:id', authenticate, authorize(['admin']), product.deleteProduct);
+router.post('/:id/addToCart', authenticate, authorize(['customer']), product.addToCart);
 
 module.exports = router;
